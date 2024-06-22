320b37779a43dd17f544f6b586fc3ea34e1ffe07
Joshua <gallopingbull89@gmail.com>
2024-06-22 15:56:49 -0700

    setting u gameboard script

---
+++ Assets/Whack-a-punk/Scripts/GameBoard.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField] GameObject molePrefab;    
+
+    public int surfaceSizeX = 0; 
+    public int surfaceSizeZ = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Init()
+    {
+
+    }
+
+    void GenerateBoard()
+    {
+
+    }
+
+    void SpawnHole(Vector3 targetPos) 
+    {
+        
+    }
+
+    void DeleteAllMoleHoles()
+    {
+
+    }
+
+}
