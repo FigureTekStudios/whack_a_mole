024db3b423d308ea44920867e959f9242128a0a1
Joshua <gallopingbull89@gmail.com>
2024-06-27 00:47:48 -0700

    Update GameManager.cs
    adding pregame counter

--- Assets/Whack-a-punk/Scripts/GameManager.cs
+++ Assets/Whack-a-punk/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public int initialCountdownTime = 60; // this should be determined by song length
+    public int preGameCountdownTime = 3; // pre-game countdown time in seconds
+    public int initialGameTime = 60; // this should be determined by song length
+    public TextMeshProUGUI preGameCountdownText;
     public TextMeshProUGUI countdownText; 
     public TextMeshProUGUI scoreText; 
 
     private int score;
-    private float countdownTimer;
+    private float gameTimer;
+    private float preGameTimer;
+    private bool gameStarted;
     private bool gameEnded;
 
     private GameBoard board; // this should probably another singleton
@@ -44,12 +49,57 @@ public class GameManager : MonoBehaviour
             InitializeGame();
         }
 
-        if (!gameEnded)
+        if (!gameStarted)
         {
-            UpdateCountdown();
+            UpdatePreGameCountdown();
         }
-
+        else if (!gameEnded)
+        {
+            UpdateGameCountdown();
+        }
     }
+
+
+    private void UpdatePreGameCountdown()
+    {
+        preGameTimer -= Time.deltaTime;
+        if (preGameTimer <= 0)
+        {
+            preGameTimer = 0;
+            StartGame();
+        }
+        UpdatePreGameCountdownText(preGameTimer);
+    }
+
+
+    private void InitializeGame()
+    {
+        score = 0;
+        gameTimer = initialGameTime;
+        preGameTimer = preGameCountdownTime;
+        gameStarted = false;
+        gameEnded = false;
+        StartCoroutine(board.GenerateGameBoard());
+
+        UpdateScoreText();
+
+        UpdatePreGameCountdownText(preGameTimer);
+    }
+
+    private void StartGame()
+    {
+        gameStarted = true;
+        preGameCountdownText.gameObject.SetActive(false); // Hide the pre-game countdown text
+        UpdateGameCountdownText(gameTimer);
+    }
+
+    private void EndGame()
+    {
+        gameEnded = true;
+        Debug.Log("Game Over! Final Score: " + score);
+        // Implement additional game over logic here (e.g., show game over screen)
+    }
+
 
     public void AddScore(int amount)
     {
@@ -60,35 +110,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void InitializeGame()
-    {
-        score = 0;
-        countdownTimer = initialCountdownTime;
-        gameEnded = false;
-        StartCoroutine(board.GenerateGameBoard());
-
-        UpdateScoreText();
-        UpdateCountdownText();
-    }
-
-    private void UpdateCountdown()
-    {
-        countdownTimer -= Time.deltaTime;
-        if (countdownTimer <= 0)
-        {
-            countdownTimer = 0;
-            EndGame();
-        }
-        UpdateCountdownText();
-    }
-
-    private void EndGame()
-    {
-        gameEnded = true;
-        Debug.Log("Game Over! Final Score: " + score);
-        // Implement additional game over logic here (e.g., show game over screen)
-    }
-
     private void UpdateScoreText()
     {
         if (scoreText != null)
@@ -97,11 +118,30 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void UpdateCountdownText()
+    private void UpdatePreGameCountdownText(float timer)
+    {
+        if (preGameCountdownText != null)
+        {
+            preGameCountdownText.text = "Game starts in: " + Mathf.CeilToInt(timer);
+        }
+    }
+
+    private void UpdateGameCountdown()
+    {
+        gameTimer -= Time.deltaTime;
+        if (gameTimer <= 0)
+        {
+            gameTimer = 0;
+            EndGame();
+        }
+        UpdateGameCountdownText(gameTimer);
+    }
+
+    private void UpdateGameCountdownText(float timer)
     {
         if (countdownText != null)
         {
-            countdownText.text = "Time: " + Mathf.CeilToInt(countdownTimer);
+            countdownText.text = "Time: " + Mathf.CeilToInt(timer);
         }
     }
 }
