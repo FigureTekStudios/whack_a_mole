312214deb8c573febd3c542fc6a5b7c75a68db62
Joshua <gallopingbull89@gmail.com>
2025-02-06 16:03:18 -0800

    updating test mouse clicker to emulate behavior from mallet
    converted test clicker into prefab
    also decreased max amount of  zombie moles

--- Assets/Whack-a-punk/Scripts/MoleHole/TestInputClicker.cs
+++ Assets/Whack-a-punk/Scripts/MoleHole/TestInputClicker.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class TestInputClicker : MonoBehaviour
 {
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Update()
     {
         // Check if the left mouse button is clicked
@@ -14,13 +20,17 @@ public class TestInputClicker : MonoBehaviour
             // Perform the raycast
             if (Physics.Raycast(ray, out hit))
             {
-                // Check if the hit object has the specific tag
-                if (hit.collider.CompareTag("MoleHole"))
-                {
-                    // Log the name of the hit GameObject
-                    Debug.Log("Clicked on: " + hit.collider.gameObject.name);
-                    hit.transform.GetComponentInParent<MoleHole>().Hit();
-                }
+                // Log the name of the hit GameObject
+                Debug.Log("Clicked on: " + hit.collider.gameObject.name);
+                IHittable hittable = hit.collider.GetComponentInParent<IHittable>(); // look at parent mole hole for this interface
+                                                               
+                if (hittable == null) { hit.collider.GetComponent<IHittable>(); }
+                
+                // Check if the hit object has the IHittable to hit      
+                if (hittable == null) return;
+
+                audioSource.Play();
+                hittable.Hit();
             }
         }
     }
